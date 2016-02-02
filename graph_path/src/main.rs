693dc1a037cc58ce8efd1de2cd63e0413f627cfa
JiaJu Ni <jiajuni@dhcp-10-105-51-93.wireless.northwestern.private>
2016-02-01 20:13:15 -0600

    writing bfs

--- graph_path/src/main.rs
+++ graph_path/src/main.rs
@@ -1,11 +1,15 @@
 use std::io::{stdin, Read, BufReader, BufRead};
 use std::fmt;
+use std::fs::File;
+use std::env;
 
 type NameTable  = std::collections::HashMap<String, usize>;
 
 struct Node {
     name:       String,
+    visited:    bool,
     distance:   usize,
+    parent:     usize,
     neighbours: Vec<usize>,
 }
 
@@ -18,7 +22,9 @@ impl Node {
     fn new(name: String) -> Self {
         Node {
             name:       name,
+            visited:    false,
             distance:   usize::max_value(),
+            parent:     usize::max_value(),
             neighbours: Vec::new()
         }
     }
@@ -38,7 +44,6 @@ impl fmt::Display for Node {
             output.push_str(&n.to_string());
             output.push(' ');
         }
-        output.push('\n');
         write!(f, "{}", output)
     }
 }
@@ -72,10 +77,10 @@ fn read_graph<R:Read>(reader: R) -> Graph {
                 is_node = false;
             } else {
                 let n = *graph.name_table.get(word).unwrap();
-                if graph.nodes[node].neighbours.contains(&n) {
+                if !graph.nodes[node].neighbours.contains(&n) {
                     graph.nodes[node].neighbours.push(n);
                 }
-                if graph.nodes[n].neighbours.contains(&node) {
+                if !graph.nodes[n].neighbours.contains(&node) {
                     graph.nodes[n].neighbours.push(node);
                 }
             }
@@ -85,8 +90,56 @@ fn read_graph<R:Read>(reader: R) -> Graph {
 }
 
 fn main() {
-    let mut graph = read_graph(stdin());
+    //argument sanity check
+    if env::args().count() != 2usize {
+        println!("\nuseage: cargo run graph.dat\n");
+    }
+
+    //open the graph description file
+    let f = match File::open(env::args().nth(1).unwrap()) {
+        Ok(file) => file,
+        Err(e)   => {
+            println!("{}", e);
+            return;
+        }
+    };
+
+    //read the graph description file and build the graph data structure
+    let mut graph = read_graph(f);
     for n in graph.nodes.iter() {
         println!("{}", n);
+    }
+
+    let mut lines = BufReader::new(stdin()).lines();
+    while let Some(Ok(line)) = lines.next() {
+        let mut nodes = line.split(" ");
+        let from_node = nodes.next().unwrap();
+        let to_node   = nodes.next().unwrap();
+        println!("from {} to {}", from_node, to_node);
+       
+        type Queue = std::collections::LinkedList<usize>;
+        let mut fifo = Queue::new();
+        fifo.push_back(match graph.name_table.get(from_node) {
+                            Some(n) => *n,
+                            None    => {
+                                println!("Invalid Node Name!");
+                                return;
+                            }
+                       });
+        while !fifo.is_empty() {
+            let current_node = fifo.pop_front().unwrap();
+            graph.nodes[current_node].visited = true;
+            for neighbour in &graph.nodes[current_node].neighbours {
+                if !graph.nodes[*neighbour].visited {
+                    continue;
+                } else {
+                    graph.nodes[*neighbour].visited = true;
+                    graph.nodes[*neighbour].parent = current_node;
+                    graph.nodes[*neighbour].distance = 
+                        graph.nodes[current_node].distance + 1;
+                    fifo.push_back(*neighbour);
+                }
+            }
+        }
     }
 }
